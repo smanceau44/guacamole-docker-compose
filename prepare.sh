022c41ef54701b0e2ba8dfbfa98f0ec8289c02e9
smanceau44 <smu@satoueb.net>
2025-02-20 14:52:07 +0100

    ppc64le with 1.5.5 Guacamole

--- prepare.sh
+++ prepare.sh
@@ -10,7 +10,7 @@ echo "Preparing folder init and creating ./init/initdb.sql"
 mkdir ./init >/dev/null 2>&1
 mkdir -p ./nginx/ssl >/dev/null 2>&1
 chmod -R +x ./init
-docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
+docker run --rm smanceau44/guacamole:1.5.5_20250220 /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
 echo "done"
 echo "Preparing folder record and set permissions"
 mkdir ./record >/dev/null 2>&1
